19f2ac7de5eaa189237fa19fba7614483b2f746a
Daisy <daisyktbpa@gmail.com>
2024-03-31 19:21:07 -0300

    "#010 fix(handlers)"

--- src/handlers/uploadFile.js
+++ src/handlers/uploadFile.js
@@ -1,28 +1,30 @@
 const multer = require('multer');
 const { generateId } = require('../helpers');
 
+// Define fileStorage before using it in the storage property
+const fileStorage = multer.diskStorage({
+  destination: (req, file, next) => {
+    next(null, `${__dirname}/../public/uploads/users/`);
+  },
+  filename: (req, file, next) => {
+    const extension = file.mimetype.split('/')[1];
+    next(null, `${generateId()}.${extension}`); // Corrected to call generateId as a function
+  },
+});
+
 const multerProfileConfiguration = {
   limits: { filesize: 10000 },
-  storage: (fileStorage = multer.diskStorage({
-    destination: (req, file, next) => {
-      next(null, `${__dirname}/../public/uploads/users/`);
-    },
-    filename: (req, file, next) => {
-      const extension = file.mimetype.split('/')[1];
-      next(null, `${generateId}.${extension}`);
-    },
-  })),
+  storage: fileStorage, // Use fileStorage directly
   fileFilter(req, file, next) {
     if (
-      file.mimetype == 'image/jpeg'
-      || file.mimetype == 'image/png'
-      || file.mimetype == 'image/jpg'
+      file.mimetype === 'image/jpeg'
+      || file.mimetype === 'image/png'
+      || file.mimetype === 'image/jpg'
     ) {
       // formato valido
       next(null, true);
     } else {
       // formato invalido
-
       next(new Error('Formato no valido'), false);
     }
   },
